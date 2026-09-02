96740821db0a809190d2974ab6ac4e5d315c1a22
sebasGZA <sebassga34@gmail.com>
2026-09-02 18:57:47 -0500

    feat: set up shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
