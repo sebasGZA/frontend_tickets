b4f815ad5904fd452f4d548fd6670cf15347b967
sebasGZA <sebassga34@gmail.com>
2026-09-02 21:55:47 -0500

    feat: implement  mobile menu

--- src/ticket/layouts/BaseLayout.tsx
+++ src/ticket/layouts/BaseLayout.tsx
@@ -4,9 +4,15 @@ import { CustomFooter } from "../components/CustomFooter"
 
 export const BaseLayout = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="flex min-h-screen flex-col bg-background">
       <CustomHeader />
-      <Outlet />
+
+      <main className="flex-1">
+        <div className="container mx-auto px-4 py-6 md:px-6 md:py-8 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+
       <CustomFooter />
     </div>
   )
