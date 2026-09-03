572934a2f25d0fe5dd33be8a347716656645c437
sebasGZA <sebassga34@gmail.com>
2026-09-03 08:53:57 -0500

    fix: remove text into custom footer

--- src/components/custom/CustomFooter.tsx
+++ src/components/custom/CustomFooter.tsx
@@ -4,9 +4,6 @@ export const CustomFooter = () => {
       <div className="container mx-auto">
         <div className="flex flex-col items-center text-center">
           <h3 className="font-semibold">Tickets.</h3>
-          <p className="text-sm text-muted-foreground mt-1">
-            Aplicación web para el manejo de tickets de soporte.
-          </p>
         </div>
 
         <div className="border-t mt-8 pt-8 text-center text-sm text-muted-foreground">
